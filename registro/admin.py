d4bf2590320066c64df96512aa18d764d34f37ac
Yuri Xavier <yuri_dsx_@hotmail.com>
2021-04-19 18:44:48 -0300

    Deleted old files

--- registro/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Register
-# Register your models here.
-
-admin.site.register(Register)
--- registro/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from registro.models import Register
-
-
-class RegisterForm(forms.ModelForm):
-    class Meta:
-        model = Register
-        fields = "__all__"
